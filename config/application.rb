11cd0b8c8e82e7c192d89298591cb726ff96e88d
Alexander Kratzsch <alex@devrandom.se>
2013-06-26 00:16:32 +0200

    hopefully fixed i18n

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Manatee
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+		config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
