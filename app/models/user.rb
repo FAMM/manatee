1b23a28b89ae9a2cc79b93838876fd79670aba2e
DarthMax <mai11inj@studserv.uni-leipzig.de>
2013-06-26 00:07:41 +0200

    added language tag to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 
   has_many :categories
   has_many :transactions
+
+  LANGUAGES=%w(en de sv)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130625203120) do
+ActiveRecord::Schema.define(version: 20130625214609) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(version: 20130625203120) do
     t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",     limit: 128, default: "", null: false
+    t.string   "email",                              default: "",   null: false
+    t.string   "encrypted_password",     limit: 128, default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20130625203120) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "language",                           default: "en"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
