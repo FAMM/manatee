1fcea229af721afaefb068d66cfa6a00eb2326e0
Alexander Kratzsch <alex@devrandom.se>
2013-06-25 22:26:46 +0200

    run initial Category migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130625202245) do
+ActiveRecord::Schema.define(version: 20130625202529) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.decimal  "budget",     precision: 20, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "transactions", force: true do |t|
     t.decimal  "amount",     precision: 20, scale: 2
