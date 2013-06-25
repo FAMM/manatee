2c38816915ddb9ecd80841a4a9031d84e9adbcb1
Alexander Kratzsch <alex@devrandom.se>
2013-06-26 01:48:56 +0200

    run AddDateToTransaction migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130625225652) do
+ActiveRecord::Schema.define(version: 20130625234601) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20130625225652) do
     t.decimal  "saldo",      precision: 20, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "date"
   end
 
   create_table "users", force: true do |t|
