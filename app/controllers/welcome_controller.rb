ed5697e9b4236525c9354c3ddc6f61869d55c333
Alexander Kratzsch <alex@devrandom.se>
2013-06-26 02:37:47 +0200

    raw stuff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
   def index
 		if current_user
-		  @transactions = current_user.transactions.includes( :category ).limit(10)
+		  # fetch last 10 transactions
+			@transactions = current_user.transactions.includes( :category ).limit(10)
+			
+			# fetch the categories for the sidebar
 			@categories = current_user.categories
 			@saldo = current_user.saldo
 			@old_saldo = current_user.saldo( Date.today.beginning_of_month - 1.day )
