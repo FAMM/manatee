aa15c6f7023f1b91b3e02b3f9b7b5b5ff9a0202d
Alexander Kratzsch <alex@devrandom.se>
2013-06-26 02:12:16 +0200

    fixed errors in fixtures, added columns needed for associations, run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130625234601) do
+ActiveRecord::Schema.define(version: 20130626000808) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
     t.decimal  "budget",     precision: 20, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "transactions", force: true do |t|
-    t.decimal  "amount",     precision: 20, scale: 2
+    t.decimal  "amount",      precision: 20, scale: 2
     t.string   "comment"
-    t.decimal  "saldo",      precision: 20, scale: 2
+    t.decimal  "saldo",       precision: 20, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "date"
+    t.integer  "user_id"
+    t.integer  "category_id"
   end
 
   create_table "users", force: true do |t|
