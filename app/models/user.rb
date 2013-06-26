dae1b72d4c104e20fbd35370e51ec5288468dacf
DarthMax <mai11inj@studserv.uni-leipzig.de>
2013-06-26 02:33:16 +0200

    user can login with email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,24 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable
 
   has_many :categories
   has_many :transactions
 
   LANGUAGES={"English" => "en", "Deutsch" => "de", "Svenska" => "se"}
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
