abfadacafe3b16b78fa29187c918cff192e300fd
Alexander Kratzsch <alex@devrandom.se>
2014-03-17 00:32:24 +0100

    converted transactions views to haml and started fixing the translations

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,2 +1,12 @@
 module TransactionsHelper
+  def criteria_to_text criteria
+		case criteria
+		when 'all'
+			t("transactions.index.all_transactions")
+		when 'user', 'category', 'date'
+			t("transactions.index.transactions_by_criteria", :criteria => @filter_criteria)
+		else
+			t("transactions.index.custom_filter")
+		end
+	end
 end
