ea08429bbcae076da7c0a60912aeee58c2e8ca4b
Alexander Kratzsch <alex@devrandom.se>
2014-03-17 15:10:51 +0100

    continued on trnaslations, added navigations to the sidebar

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,12 +1,12 @@
 module TransactionsHelper
   def criteria_to_text criteria
 		case criteria
-		when 'all'
-			t("transactions.index.all_transactions")
 		when 'user', 'category', 'date'
 			t("transactions.index.transactions_by_criteria", :criteria => @filter_criteria)
-		else
+		when 'custom'
 			t("transactions.index.custom_filter")
+		else
+			t("transactions.index.all_transactions")
 		end
 	end
 end
