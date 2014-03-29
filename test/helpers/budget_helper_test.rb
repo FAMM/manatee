2acf6ad4c55f9f9322a22c3e6e05e54f91a6cb3c
DarthMax <max@kopfueber.org>
2014-03-29 21:53:05 +0100

    remove all test/unit related files

--- test/helpers/budget_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class BudgetHelperTest < ActionView::TestCase
-end
--- test/models/budgets_users_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class BudgetsUsersTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
